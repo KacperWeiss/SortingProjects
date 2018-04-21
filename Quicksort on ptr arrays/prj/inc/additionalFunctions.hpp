80a26f70686bd26f6020194c6bfd725c14be55f3
Kacper Weiss <kacper.weiss@gmail.com>
2018-04-21 22:11:16 +0200

    Additional functions added for quicksort

---
+++ Quicksort on ptr arrays/prj/inc/additionalFunctions.hpp
@@ -0,0 +1,78 @@
+#ifndef ADDITIONAL_FUNCTIONS_QS
+#define ADDITIONAL_FUNCTIONS_QS
+
+#include "quicksort.hpp"
+#include <iostream>
+#include <cstring>
+
+/*!
+ * \brief Function that shows menu
+ * 
+ * Function that shows menu to an user, within itself function uses
+ * std namespace.
+ */
+bool showMenu(){
+
+    using namespace std;
+
+    cout    << endl
+            << "a - sort ascending\n"
+            << "d - sort descending\n"
+            << "r - make elements random\n"
+            << "t - quick test\n\n"
+            << "m - show menu\n"
+            << "q - quit program\n"
+            << endl;
+
+    return true;
+    
+}
+
+/*!
+ * \brief Sorts elements in ascending order.
+ * 
+ * Function that sort elements in ascending order.
+ */
+template <class T>
+bool ascendingQS(T tab[], int length){
+
+    if(tab == NULL)
+        return false;
+
+    return quickSort(tab, 0, length-1);
+
+}
+
+/*!
+ * \brief Sorts elements in descending order.
+ * 
+ * Function that sort elements in descending order.
+ */
+template <class T>
+bool descendingQS(T tab[], int length){
+
+    if(tab == NULL)
+        return false;
+
+    return quickSortReverse(tab, 0, length-1);
+
+}
+
+/*!
+ * \brief Adds random elements.
+ * 
+ * Function that adds random elements into choosen tabtor.
+ */
+template <class T>
+bool addRandomValues(T tab[], int start, int length){
+
+    srand(time(nullptr));
+
+    for(int i = start; i < length; i++)
+        tab[i] = (rand() * rand());
+
+    return true;
+
+}
+
+#endif
